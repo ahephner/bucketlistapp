d5806b905978efe436eb947d1523306b9e4fa27c
AJ Hephner <ajhephner178@gmail.com>
2017-09-19 11:56:53 -0400

    11.2 done status unknown

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import  { Component } from 'react';
-import NavBarHeader from './Nav';
-
+import NavBarHeader from './nav';
+import Video from './video/video';
 
 	export default class App extends Component{
 		render(){
 			return (
 				  <div>
 				  		<NavBarHeader />
+				  		<Video />
 				  </div>
 				);
 		}
