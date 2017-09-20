596e456399cbdd05ff41821b9756ad2f70039379
AJ Hephner <ajhephner178@gmail.com>
2017-09-20 16:58:36 -0400

    14 not sure its ok but the module said i should get an error

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,14 +1,24 @@
 import axios from 'axios';
 
+// export const CREATE_POSTS = 'CREATE_POSTS';
+
+// const ROOT_URL = 'http://rest.learn.code.academy/api/aj';
+
+const ROOT_URL = 'http://localhost:3000';
 export const CREATE_POSTS = 'CREATE_POSTS';
 
-const Root_URL = 'http://rest.learn.code.academy/api/aj';
-
 export function createPost(props){
-	//ROOT_URL 
+
 	const request = axios.post(`${Root_URL}/posts`, props);
 	return{
 		type: CREATE_POSTS,
 		payload: request
 	};
+}
+
+export function signinUser ({email, password}){
+	return function(dispatch){
+		axios.post(`${ROOT_URL}/signin`, {email, password})
+
+	}
 }
