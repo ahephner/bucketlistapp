e10a3511adc99a3d294ca6fc6468267f7f88feeb
AJ Hephner <ajhephner178@gmail.com>
2017-09-21 15:50:50 -0400

    finished 21 all working

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -10,23 +10,45 @@ import authReducer from '../reducers/auth_reducer';
 
 
 export const CREATE_POSTS = 'CREATE_POSTS';
-//const ROOT_URL = 'http://rest.learncode.academy/api/aj';
+//const ROOT_URL = 'http://rest.learncode.academy/api/paul';
 const ROOT_URL = 'http://localhost:3000';
 
 
 export function signinUser({ email, password }){
-	return function(dispatch){
-		axios.post(`${ROOT_URL}/signin`, {email, password})
- 			.then(response => {
- 		
- 				dispatch({ type: AUTH_USER });
- 				localStorage.setItem('token', response.data.token);
- 				browserHistory.push('/newitem');
-      	
-      	 })
-      		.catch(response =>  dispatch(authError("There was a something wrong with your request.")));
-	}
+  return function(dispatch){
+    axios.post(`${ROOT_URL}/signin`, {email, password})
+      .then(response => {
+    
+        dispatch({ type: AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+        browserHistory.push('/newitem');
+        
+         })
+          .catch(response =>  dispatch(authError("There was a something wrong with your request.")));
+  }
 }
+
+export function signoutUser(){
+   localStorage.removeItem('token'); 
+   return {type: UNAUTH_USER};
+}
+
+export function signupUser({ email, password }) {
+  return function(dispatch) {
+    // Submit email/password to the server
+    axios.post(`${ROOT_URL}/signup`, { email, password })
+      .then(response => {
+        dispatch({type: AUTH_USER});
+          
+          //update the token
+          localStorage.setItem('token', response.data.token);
+          browserHistory.push('/newitem');
+      })
+      .catch(response => dispatch(authError(response.data.error)));
+  }
+}
+
+
 
 
 export function authError(error) {
@@ -38,15 +60,9 @@ export function authError(error) {
 
 
 export function createPost(props) {
-	const request = axios.post(`${ROOT_URL}/posts`, props);						
-	return {
-		type: CREATE_POSTS,
-		payload: request
-	};
-}
-
-export function signoutUser(){
-  localStorage.removeItem('token');
-
-  return {type: UNAUTH_USER};
+  const request = axios.post(`${ROOT_URL}/posts`, props);           
+  return {
+    type: CREATE_POSTS,
+    payload: request
+  };
 }
